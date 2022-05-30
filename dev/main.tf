0090946f9a45ad8a89679e009c2637922a4e334c
Taehyun Kim <76610403+k1m743hyun@users.noreply.github.com>
2022-05-30 17:12:11 +0900

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -12,7 +12,8 @@ provider "aws" {
 }
 
 resource "aws_vpc" "main" {
-    cidr_block = "10.0.0.0/16"
+    vpc_id      = aws_vpc.main.id
+    cidr_block  = "10.0.0.0/16"
 
     tags = {
         Name = "dev"
