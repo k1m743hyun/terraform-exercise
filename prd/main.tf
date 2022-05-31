d1daa0402b30973d61a0de94c71f630180ac3171
Taehyun Kim <k1m743hyun@icloud.com>
2022-05-31 01:52:16 +0000

    Update prd/main.tf

--- prd/main.tf
+++ prd/main.tf
@@ -12,14 +12,39 @@ provider "aws" {
 }
 
 resource "aws_vpc" "AILab-vpc" {
-    cidr_block = "172.31.0.0/16"
+    cidr_block = "172.10.0.0/16"
 
     tags = {
         Name = "AILab-vpc-prd"
     }
 }
 
-resource "aws_subnet" "main" {
+resource "aws_subnet" "AILab-subnet-prd-a" {
     vpc_id      = aws_vpc.AILab-vpc.id
-    cidr_block  = "172.31.0.0/24"
+    cidr_block  = "172.10.0.0/24"
+    availability_zone = "${var.region}a"
+
+    tags = {
+        Name = "AILab-subnet-prd-a"
+    }
+}
+
+resource "aws_subnet" "AILab-subnet-prd-c" {
+    vpc_id      = aws_vpc.AILab-vpc.id
+    cidr_block  = "172.10.2.0/24"
+    availability_zone = "${var.region}c"
+
+    tags = {
+        Name = "AILab-subnet-prd-c"
+    }
+}
+
+resource "aws_subnet" "AILab-subnet-prd-d" {
+    vpc_id      = aws_vpc.AILab-vpc.id
+    cidr_block  = "172.10.3.0/24"
+    availability_zone = "${var.region}d"
+
+    tags = {
+        Name = "AILab-subnet-prd-d"
+    }
 }
