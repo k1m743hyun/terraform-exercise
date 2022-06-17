7fb63f7a5e430680bc954f86791468d4fd48433c
Taehyun Kim <k1m743hyun@icloud.com>
2022-06-17 06:10:07 +0000

    Code Refactoring

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-    source                    = "./vpc"
+    source                    = "../../modules/vpc"
     vpc_cidr_block            = "10.0.0.0/16"
     public_subnet_cidr_block  = "10.0.0.0/24"
     private_subnet_cidr_block = "10.0.1.0/24"
