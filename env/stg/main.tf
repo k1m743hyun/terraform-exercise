ff11ef845da89d2b4c7e0ac01f4d97b7da85caef
Taehyun Kim <k1m743hyun@icloud.com>
2022-06-17 06:22:42 +0000

    Create stg Environment

---
+++ env/stg/main.tf
@@ -0,0 +1,6 @@
+module "vpc" {
+    source                    = "../../modules/vpc"
+    vpc_cidr_block            = "10.0.0.0/16"
+    public_subnet_cidr_block  = "10.0.0.0/24"
+    private_subnet_cidr_block = "10.0.1.0/24"
+}
