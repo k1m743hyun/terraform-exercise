ab0da8edc7fb1d41d61f32a188636f989a12bafd
Taehyun Kim <k1m743hyun@icloud.com>
2022-06-22 07:17:21 +0000

    Update modules/vpc/main.tf

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -4,30 +4,18 @@ data "aws_availability_zones" "available" {
 
 resource "aws_vpc" "main" {
     cidr_block = var.vpc_cidr_block
-
-    tags = {
-        Name = "AILab-vpc"
-    }
 }
 
 resource "aws_subnet" "public" {
     vpc_id            = aws_vpc.main.id
     cidr_block        = var.public_subnet_cidr_block
     availability_zone = "${data.aws_availability_zones.available.names[0]}"
-
-    tags = {
-        Name = "AILab-subnet-public"
-    }
 }
 
 resource "aws_subnet" "private" {
     vpc_id            = aws_vpc.main.id
     cidr_block        = var.private_subnet_cidr_block
     availability_zone = "${data.aws_availability_zones.available.names[0]}"
-
-    tags = {
-        Name = "AILab-subnet-private"
-    }
 }
 
 resource "aws_internet_gateway" "public" {
