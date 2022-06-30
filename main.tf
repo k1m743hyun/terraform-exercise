3fe74aec5699bbfcbd84a3f5289567aaa437bdf7
Taehyun Kim <k1m743hyun@icloud.com>
2022-06-30 07:49:16 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+  ami                   = "ami-0c55b159cbfafe1f0"
+  instance_type         = "t2.micro"
   vpc_security_group_id = [aws_security_group.instance.id]
 
   user_data = <<-EOF
@@ -22,9 +22,9 @@ resource "aws_security_group" "instance" {
   name = "terraform-example-instance"
 
   ingress {
-    from_port = 8080
-    to_port = 8080
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    from_port     = 8080
+    to_port       = 8080
+    protocol      = "tcp"
+    cidr_blocks   = ["0.0.0.0/0"]
   }
 }
