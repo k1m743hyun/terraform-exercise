89a57bd1bbb458a51c220c1abbf0a5c9d2fe02fd
Taehyun Kim <k1m743hyun@icloud.com>
2022-06-30 08:54:43 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
-resource "aws_instance" "example" {
-  ami                   = "ami-0c55b159cbfafe1f0"
-  instance_type         = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.instance.id]
+resource "aws_launch_configuration" "example" {
+  image_id               = "ami-0c55b159cbfafe1f0"
+  instance_type          = "t2.micro"
+
+  security_groups = [aws_security_group.instance.id]
 
   user_data = <<-EOF
                 #!/bin/bash
@@ -9,8 +10,9 @@ resource "aws_instance" "example" {
                 nohup busybox httpd -f -p ${var.server_port} &
                 EOF
 
-  tags = {
-    Name = "terraform-exercise"
+  # ASG에서 시작 구성을 사용할 때 필요합니다
+  lifecycle {
+    create_before_destroy = true
   }
 }
 
@@ -22,5 +24,19 @@ resource "aws_security_group" "instance" {
     to_port       = var.server_port
     protocol      = "tcp"
     cidr_blocks   = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = aws_launch_configuration.example.name
+  vpc_zone_identifier  = data.aws_subnet_ids.default.ids
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-asg-example"
+    propagate_at_launch = true
   }
 }
