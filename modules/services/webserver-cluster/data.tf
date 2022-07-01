654792a491ca4d8a2d3b43c1fd2224c548c80ef3
Taehyun Kim <k1m743hyun@icloud.com>
2022-07-01 06:13:00 +0000

    Update modules/services/webserver-cluster/data.tf

--- modules/services/webserver-cluster/data.tf
+++ modules/services/webserver-cluster/data.tf
@@ -1,21 +1,3 @@
-data "terraform_remote_state" "db" {
-  backend = "s3"
-
-  config = {
-    bucket = var.db_remote_state_bucket
-    key    = var.db_remote_state_key
-    region = var.region
-  }
-}
-
-locals {
-  http_port    = 80
-  any_port     = 0
-  any_protocol = "-1"
-  tcp_protocol = "tcp"
-  all_ips      = ["0.0.0.0/0"]
-}
-
 data "aws_vpc" "default" {
   default = true
 }
