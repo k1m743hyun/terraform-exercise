705c633859e454c9946caaefcdf1baff470d487b
Taehyun Kim <k1m743hyun@icloud.com>
2022-07-01 06:08:27 +0000

    Update dev/variables.tf

--- dev/variables.tf
+++ dev/variables.tf
@@ -8,16 +8,6 @@ variable "region" {
   description = "AWS region"
 }
 
-variable "db_remote_state_bucket" {
-  description = "The name of the S3 bucket used for the database's remote state storage"
-  type        = string
-}
-
-variable "db_remote_state_key" {
-  description = "The name of the key in the S3 bucket used for the database's remote state storage"
-  type        = string
-}
-
 # ---------------------------------------------------------------------------------------------------------------------
 # OPTIONAL PARAMETERS
 # These parameters have reasonable defaults.
