c013f3828839ec05fd26eb69a4db80dda99ff3e2
Taehyun Kim <k1m743hyun@icloud.com>
2022-07-01 02:31:30 +0000

    Update all files

--- variables.tf
+++ variables.tf
@@ -1,53 +1,71 @@
-# ---------------------------------------------------------------------------------------------------------------------
-# REQUIRED PARAMETERS
-# You must provide a value for each of these parameters.
-# ---------------------------------------------------------------------------------------------------------------------
+variable "region" {
+  description   = "AWS region"
+  type          = string
+}
 
-#variable "db_remote_state_bucket" {
-#  description = "The name of the S3 bucket used for the database's remote state storage"
-#  type        = string
+variable "number_example" {
+  description   = "An example of a number variable in Terraform"
+  type          = number
+  default       = 42
+}
+
+variable "list_example" {
+  description   = "An example of a list in Terraform"
+  type          = list
+  default       = ["a", "b", "c"]
+}
+
+variable "list_numeric_example" {
+  description   = "An example of a numeric list in Terraform"
+  type          = list(number)
+  default       = [1, 2, 3]
+}
+
+variable "map_example" {
+  description   = "An example of a map in Terraform"
+  type          = map(string)
+
+  default = {
+    key1 = "value1"
+    key2 = "value2"
+    key3 = "value3"
+  }
+}
+
+variable "object_example" {
+  description   = "An example of a structural type in Terraform"
+  type          = object({
+    name        = string
+    age         = number
+    tags        = list(string)
+    enabled     = bool
+  })
+  default = {
+    name        = "value1"
+    age         = 42
+    tags        = ["a", "b", "c"]
+    enabled     = true
+  }
+}
+
+#variable "object_example_with_error" {
+#  description   = "An example of a structural type in Terraform with an error"
+#  type          = object({
+#    name        = string
+#    age         = number
+#    tags        = list(string)
+#    enabled     = bool
+#  })
+#  default = {
+#    name        = "value1"
+#    age         = 42
+#    tags        = ["a", "b", "c"]
+#    enabled     = "invalid"
+#  }
 #}
-
-#variable "db_remote_state_key" {
-#  description = "The name of the key in the S3 bucket used for the database's remote state storage"
-#  type        = string
-#}
-
-# ---------------------------------------------------------------------------------------------------------------------
-# OPTIONAL PARAMETERS
-# These parameters have reasonable defaults.
-# ---------------------------------------------------------------------------------------------------------------------
 
 variable "server_port" {
-  description = "The port the server will use for HTTP requests"
-  type        = number
-  default     = 8080
-}
-
-variable "alb_name" {
-  description = "The name of the ALB"
-  type        = string
-  default     = "terraform-asg-example"
-}
-
-variable "instance_security_group_name" {
-  description = "The name of the security group for the EC2 Instances"
-  type        = string
-  default     = "terraform-example-instance"
-}
-
-variable "alb_security_group_name" {
-  description = "The name of the security group for the ALB"
-  type        = string
-  default     = "terraform-example-alb"
-}
-
-variable "bucket_name" {
-  description = "The name of the S3 bucket. Must be globally unique."
-  type        = string
-}
-
-variable "table_name" {
-  description = "The name of the DynamoDB table. Must be unique in this AWS account."
-  type        = string
-}
+  description   = "The port the server will use for HTTP requests"
+  type          = number
+  default       = 8080
+} 
