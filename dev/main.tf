022c1375bf9fc1c8be373920a284e1a9025ffe8e
Taehyun Kim <k1m743hyun@icloud.com>
2022-07-04 04:47:42 +0000

    Update dev/main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -1,3 +1,11 @@
+locals {
+  http_port    = 12345
+  any_port     = 0
+  tcp_protocol = "tcp"
+  any_protocol = "-1"
+  all_ips      = ["0.0.0.0/0"]
+}
+
 module "webserver_cluster" {
   source        = "../modules/services/webserver-cluster"
 
@@ -12,8 +20,8 @@ resource "aws_security_group_rule" "allow_testing_inbound" {
   type              = "ingress"
   security_group_id = module.webserver_cluster.alb_security_group_id
 
-  from_port         = 12345
-  to_port           = 12345
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
+  from_port         = local.http_port
+  to_port           = local.http_port
+  protocol          = local.tcp_protocol
+  cidr_blocks       = local.all_ips
 }
