8e23b99b5328f79d908f464025db9fcd466f0de3
Taehyun Kim <k1m743hyun@icloud.com>
2022-07-05 07:42:54 +0000

    Add the route table at dev

--- dev/variables.tf
+++ dev/variables.tf
@@ -8,13 +8,12 @@ variable "region" {
   type        = string
 }
 
+variable "environment" {
+  description = "AWS environment"
+  type        = string
+}
+
 # ---------------------------------------------------------------------------------------------------------------------
 # OPTIONAL PARAMETERS
 # These parameters have reasonable defaults.
 # ---------------------------------------------------------------------------------------------------------------------
-
-variable "environment" {
-  description = "Environment"
-  type        = string
-  default     = "dev"
-}
