09d050db2cc89eadde58a041721977676f0958ff
Taehyun Kim <k1m743hyun@icloud.com>
2022-07-05 06:10:33 +0000

    Update modules

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,4 +1,8 @@
 output "vpc_id" {
-  description = "The ID of the VPC"
+  description = "The ID of the AWS VPC"
   value       = aws_vpc.this.id
+}
+
+output "subnet" {
+  value = { for k, sbn in aws_subnet.this : sbn.tags.Name => sbn.id }
 }
