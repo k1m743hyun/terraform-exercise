e199db151e1376cdd9900a07dea334cca6686b32
Taehyun Kim <k1m743hyun@icloud.com>
2022-07-05 06:32:35 +0000

    Update stg/main.tf

--- stg/main.tf
+++ stg/main.tf
@@ -5,12 +5,15 @@ module "vpc" {
 
   subnets = {
     "a" = {
+      name       = "a"
       cidr_block = "10.0.0.0/24"
     },
     "b" = {
+      name       = "b"
       cidr_block = "10.0.1.0/24"
     },
     "c" = {
+      name       = "c"
       cidr_block = "10.0.2.0/24"
     }
   }
