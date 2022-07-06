e047bd26b559efbe4a590e41c84189d65465a7c2
Taehyun Kim <k1m743hyun@icloud.com>
2022-07-06 04:45:50 +0000

    Update modules/vpc

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -1,9 +1,21 @@
-variable "vpc_name" {}
+variable "vpc_name" {
+  description = "Name to be used on all the resources as identifier"
+  type        = string
+}
 
-variable "vpc_cidr" {}
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC. Default value is a valid CIDR, but not acceptable by AWS and should be overriden"
+  type        = string
+}
 
-variable "subnets" {}
+variable "subnets" {
+  description = "A list of subnets inside the VPC"
+  type        = list(string)
+}
 
-variable "route_table" {}
+variable "route_table" {
+  description = "A list of route tables inside the VPC"
+  type        = list(string)
+}
 
 variable "tags" {}
