174c29825f10f137e0fb7aef0d38cd11b6ca281e
Taehyun Kim <k1m743hyun@icloud.com>
2022-09-13 11:39:54 +0900

    Create directory-structure/for-environment/modules/network

---
+++ directory-structure/for-environment/modules/network/subnet.tf
@@ -0,0 +1,27 @@
+# Subnet 생성
+resource "aws_subnet" "this" {
+  for_each = var.subnets
+
+  count = length(each.value) # 여러개를 정의합니다
+  
+  vpc_id            = aws_vpc.this.id
+  cidr_block        = each.value[count.index]
+  availability_zone = var.azs[count.index]
+  
+  tags = merge(
+    {
+      Name = "sbn-${var.tags.Environment}-${each.value}-${count.index + 1}"
+    },
+    var.tags
+  )
+}
+
+# 프라이빗 서브넷을 라우팅 테이블에 연결합니다
+#resource "aws_route_table_association" "private" {
+#  for_each = var.subnets
+
+#  count = length(each.value)
+
+#  subnet_id      = aws_subnet.this[count.index].id
+#  route_table_id = aws_route_table.this.id
+#}
