81adbfe1984b2b0af13e93e9324e723e03ce0334
Taehyun Kim <k1m743hyun@icloud.com>
2022-09-15 10:39:09 +0900

    Create structure-for-environment/modules/database

---
+++ structure-for-environment/modules/database/instance.tf
@@ -0,0 +1,25 @@
+# Aurora Postgres Instance 생성
+resource "aws_rds_cluster_instance" "this" {
+  for_each                     = { for k, v in var.rds_value : join("-", [k, lookup(v, "multi_az", [])]) => v }
+  identifier                   = format("rdsinst-${var.tags.Owner}-${var.tags.Project}-${var.tags.Environment}-%s-01-az2a", each.value.cluster_identifier)
+  cluster_identifier           = aws_rds_cluster.this[split("-", each.key, 0)].cluster_identifier
+  db_parameter_group_name      = aws_db_parameter_group.rds_instance_parmetg.name
+  instance_class               = each.value.instance_class
+  engine                       = "aurora-postgresql"
+  engine_version               = lookup(each.value, "engine_version", "13.3")
+  availability_zone            = "ap-northeast-2a"
+  performance_insights_enabled = true
+  apply_immediately            = each.value.apply_immediately
+  auto_minor_version_upgrade   = false
+  monitoring_role_arn          = lookup(each.value, "monitoring_role_arn", null)
+  monitoring_interval          = lookup(each.value, "monitoring_interval", null)
+
+  tags = merge(
+    {
+      Name    = format("rdsinst-${var.tags.Owner}-${var.tags.Project}-${var.tags.Environment}-%s-01-az2a", each.value.cluster_identifier)
+      Type    = "rdsinst"
+      Purpose = "postgres"
+    },
+    var.tags
+  )
+}
