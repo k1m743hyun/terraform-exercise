7c0c9badd1c95c90947fa2f27def20db2f90470c
Taehyun Kim <k1m743hyun@icloud.com>
2022-09-16 10:06:18 +0900

    Update structure-for-environment/modules/database/instance.tf

--- structure-for-environment/modules/database/instance.tf
+++ structure-for-environment/modules/database/instance.tf
@@ -1,22 +1,22 @@
 # Aurora Postgres Instance 생성
 resource "aws_rds_cluster_instance" "this" {
-  for_each                     = var.rds_value
-  identifier                   = format("rdsinst-${var.tags.Environment}-%s-01-${element(split("-", each.key), 1)}", each.value.cluster_identifier)
-  cluster_identifier           = aws_rds_cluster.this[element(split("-", each.key), 0)].cluster_identifier
+  for_each                     = var.rds_az
+  identifier                   = format("rdsinst-${var.tags.Environment}-%s-01-${element(split("-", each.value), 1)}", var.rds_value[element(split("-", each.value), 0)].cluster_identifier)
+  cluster_identifier           = aws_rds_cluster.this[element(split("-", each.value), 0)].cluster_identifier
   db_parameter_group_name      = aws_db_parameter_group.rds_instance_parmetg.name
-  instance_class               = each.value.instance_class
+  instance_class               = var.rds_value[element(split("-", each.value), 0)].instance_class
   engine                       = "aurora-postgresql"
-  engine_version               = lookup(each.value, "engine_version", "13.3")
-  availability_zone            = "ap-northeast-2${substr(element(split("-", each.key), 1), -1, -1)}"
+  engine_version               = lookup(var.rds_value[element(split("-", each.value), 0)], "engine_version", "13.3")
+  availability_zone            = "ap-northeast-2${substr(element(split("-", each.value), 1), -1, -1)}"
   performance_insights_enabled = true
-  apply_immediately            = each.value.apply_immediately
+  apply_immediately            = var.rds_value[element(split("-", each.value), 0)].apply_immediately
   auto_minor_version_upgrade   = false
-  monitoring_role_arn          = lookup(each.value, "monitoring_role_arn", null)
-  monitoring_interval          = lookup(each.value, "monitoring_interval", null)
+  monitoring_role_arn          = lookup(var.rds_value[element(split("-", each.value), 0)], "monitoring_role_arn", null)
+  monitoring_interval          = lookup(var.rds_value[element(split("-", each.value), 0)], "monitoring_interval", null)
 
   tags = merge(
     {
-      Name    = format("rdsinst-${var.tags.Environment}-%s-01-${element(split("-", each.key), 1)}", each.value.cluster_identifier)
+      Name    = format("rdsinst-${var.tags.Environment}-%s-01-${element(split("-", each.value), 1)}", var.rds_value[element(split("-", each.value), 0)].cluster_identifier)
       Type    = "rdsinst"
       Purpose = "postgres"
     },
