fac707d744b6f477a7945c25b9a165255cc54721
Taehyun Kim <k1m743hyun@icloud.com>
2022-09-20 12:23:09 +0900

    Update add structure-for-environment/modules/database/subnet_group.tf

--- structure-for-environment/modules/database/subnet_group.tf
+++ structure-for-environment/modules/database/subnet_group.tf
@@ -3,6 +3,10 @@ resource "aws_db_subnet_group" "rds_sbng" {
   name       = "sbng-${var.tags.Environment}-rds"
   subnet_ids = var.subnet_ids
 
+  depends_on = [
+    aws_rds_cluster_instance.this
+  ]
+
   tags = merge(
     {
       Name = "sbng-${var.tags.Environment}-rds"
