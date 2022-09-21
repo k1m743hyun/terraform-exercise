15a7852d76070fe4d8b75152f9d172b2215c62cb
Taehyun Kim <k1m743hyun@icloud.com>
2022-09-21 13:11:14 +0900

    Update structure-for-environment/modules/database/

--- structure-for-environment/modules/database/security_group.tf
+++ structure-for-environment/modules/database/security_group.tf
@@ -1,7 +1,6 @@
 # Security Group
-resource "aws_security_group" "sg_rds" {
-  name = "seg-${var.tags.Environment}-rds"
-
+resource "aws_security_group" "this" {
+  name   = "seg-${var.tags.Environment}-rds"
   vpc_id = var.vpc_id
 
   tags = merge(
@@ -14,32 +13,34 @@ resource "aws_security_group" "sg_rds" {
 }
 
 # Security Group Rule
-resource "aws_security_group_rule" "sg_rds_cidr" {
+resource "aws_security_group_rule" "this" {
   for_each          = var.sg_rds_cidr
+
   type              = each.value.type
   from_port         = each.value.from_port
   to_port           = each.value.to_port
   protocol          = each.value.protocol
   cidr_blocks       = [each.value.cidr_blocks]
-  security_group_id = aws_security_group.sg_rds.id
+  security_group_id = aws_security_group.this.id
   description       = each.value.description
 
   depends_on = [
-    aws_security_group.sg_rds
+    aws_security_group.this
   ]
 }
 
-resource "aws_security_group_rule" "sg_rds_source" {
-  for_each                 = var.sg_rds_source
-  type                     = each.value.type
-  from_port                = each.value.from_port
-  to_port                  = each.value.to_port
-  protocol                 = each.value.protocol
-  source_security_group_id = each.value.source
-  security_group_id        = aws_security_group.sg_rds.id
-  description              = each.value.description
+#resource "aws_security_group_rule" "sg_rds_source" {
+#  for_each                 = var.sg_rds_source
 
-  depends_on = [
-    aws_security_group.sg_rds
-  ]
-}
+#  type                     = each.value.type
+#  from_port                = each.value.from_port
+#  to_port                  = each.value.to_port
+#  protocol                 = each.value.protocol
+#  source_security_group_id = each.value.source
+#  security_group_id        = aws_security_group.sg_rds.id
+#  description              = each.value.description
+
+#  depends_on = [
+#    aws_security_group.sg_rds
+#  ]
+#}
