73991305f842aa020c67dfe3255b3626c812d2f3
Taehyun Kim <k1m743hyun@icloud.com>
2022-09-23 16:07:34 +0900

    Rename modules/vpc

--- structure-for-environment/modules/vpc/subnet.tf
+++ structure-for-environment/modules/vpc/subnet.tf
@@ -1,10 +1,10 @@
 # Subnet 생성
 resource "aws_subnet" "this" {
-  count = length(var.subnets)
+  count             = length(var.subnets)
 
   vpc_id            = aws_vpc.this.id
   cidr_block        = element(split("-", var.subnets[count.index]), 1)
-  availability_zone = var.availability_zone[count.index % 3]
+  availability_zone = var.availability_zones[count.index % (length(var.subnets) / length(var.public_subnets))]
 
   depends_on = [
     aws_vpc.this
@@ -21,7 +21,7 @@ resource "aws_subnet" "this" {
 
 # Subnet을 Route table에 연결
 resource "aws_route_table_association" "this" {
-  count = length(var.subnets)
+  count          = length(var.subnets)
 
   subnet_id      = aws_subnet.this[count.index].id
   route_table_id = aws_route_table.this[element(split("-", var.subnets[count.index]), 0)].id
