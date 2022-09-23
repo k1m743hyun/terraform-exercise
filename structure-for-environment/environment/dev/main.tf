d1065b0bae3affdd1b1cd6aa98ffa213fc9e1ca9
Taehyun Kim <k1m743hyun@icloud.com>
2022-09-23 17:03:47 +0900

    Update structure-for-environment/environment/dev/main.tf

--- structure-for-environment/environment/dev/main.tf
+++ structure-for-environment/environment/dev/main.tf
@@ -21,26 +21,26 @@ module "vpc" {
   }
 }
 
-module "rds" {
-  source              = "../../modules/rds"
+# module "rds" {
+#   source              = "../../modules/rds"
 
-  region              = var.region
-  vpc_id              = module.vpc.vpc_id
+#   region              = var.region
+#   vpc_id              = module.vpc.vpc_id
 
-  rds_az              = flatten([ for k, v in var.rds_config : [ for az in lookup(var.rds_config[k], "multi_az") : join("-", [k, az]) ] ])
-  rds_value           = var.rds_config
-  sg_rds_cidr         = var.sg_rds_cidr
-  sg_rds_source       = var.sg_rds_source
+#   rds_az              = flatten([ for k, v in var.rds_config : [ for az in lookup(var.rds_config[k], "multi_az") : join("-", [k, az]) ] ])
+#   rds_value           = var.rds_config
+#   sg_rds_cidr         = var.sg_rds_cidr
+#   sg_rds_source       = var.sg_rds_source
 
-  rds_master_username = var.rds_master_username
-  rds_master_password = var.rds_master_password
+#   rds_master_username = var.rds_master_username
+#   rds_master_password = var.rds_master_password
 
-  subnet_ids          = module.vpc.private_subnet_ids
+#   subnet_ids          = module.vpc.private_subnet_ids
 
-  tags = {
-    Environment = var.environment
-  }
-}
+#   tags = {
+#     Environment = var.environment
+#   }
+# }
 
 module "ecr" {
   source    = "../../modules/ecr"
